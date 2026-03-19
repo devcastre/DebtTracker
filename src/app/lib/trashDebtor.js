9c3f69e35f4fefb71895f483ca94b3e03e87bfc8
Renato Castillo <castillopayat@gmail.com>
2026-03-19 15:45:02 +0800

    adding payment transaction for accident trash or forcely trash debtor with balance

--- src/app/lib/trashDebtor.js
+++ src/app/lib/trashDebtor.js
@@ -4,16 +4,46 @@ import { supabase } from "./supabase";
 
 export default function useTrashDebtor() {
 
-    const trashDebtor = async (id) => {
+    const now = new Date();
 
-        const {data, error} = await supabase
-            .from('debtors')
-            .update({status: 'trash'})
-            .eq('id', id)
+    const localDateTime = now.getFullYear() + '-' +
+    String(now.getMonth() + 1).padStart(2, '0') + '-' +
+    String(now.getDate()).padStart(2, '0') + ' ' +
+    String(now.getHours()).padStart(2, '0') + ':' +
+    String(now.getMinutes()).padStart(2, '0') + ':' +
+    String(now.getSeconds()).padStart(2, '0');
 
-        if(error) throw error  
+    const trashDebtor = async (id, balance) => {
 
-        return data;       
+        try {
+
+            if (balance > 0){
+                await supabase
+                .from('transactions')
+                .insert([
+                    {
+                    debtor_id: id,
+                    type: 'payment',
+                    amount: balance,
+                    date: localDateTime,
+                    interest_rate: 5
+                    }
+                ]);
+            }        
+
+            const {data, error} = await supabase
+                .from('debtors')
+                .update({status: 'trash'})
+                .eq('id', id)
+
+            if(error) throw error  
+
+            return data;
+
+        } catch (error) {
+            console.error("Error trashing debtor:", err);
+            throw err;            
+        }
     }
 
     return {trashDebtor};
